d8361fce95fa772c7e57c6db2e5daf7bd9c95fb5
MG <mgding@dotdotbuy.com>
2017-02-12 23:21:24 +0800

    update: 替换chapter.js

--- pages/index/chapter.bak.js
+++ pages/index/chapter.bak.js
@@ -4,140 +4,140 @@ module.exports = [
             "short_name": "创",
             "full_name": "创世记",
             "is_new": 0,
-            "chapter_number": 50
+            "chapter_number": 31
         },
         {
             "volume_id": 2,
             "short_name": "出",
             "full_name": "出埃及记",
             "is_new": 0,
-            "chapter_number": 40
+            "chapter_number": 35
         },
         {
             "volume_id": 3,
             "short_name": "利",
             "full_name": "利未记",
             "is_new": 0,
-            "chapter_number": 27
+            "chapter_number": 57
         },
         {
             "volume_id": 4,
             "short_name": "民",
             "full_name": "民数记",
             "is_new": 0,
-            "chapter_number": 36
+            "chapter_number": 29
         },
         {
             "volume_id": 5,
             "short_name": "申",
             "full_name": "申命记",
             "is_new": 0,
-            "chapter_number": 34
+            "chapter_number": 12
         },
         {
             "volume_id": 6,
             "short_name": "书",
             "full_name": "约书亚记",
             "is_new": 0,
-            "chapter_number": 24
+            "chapter_number": 15
         },
         {
             "volume_id": 7,
             "short_name": "士",
             "full_name": "士师记",
             "is_new": 0,
-            "chapter_number": 21
+            "chapter_number": 20
         },
         {
             "volume_id": 8,
             "short_name": "得",
             "full_name": "路得记",
             "is_new": 0,
-            "chapter_number": 4
+            "chapter_number": 23
         },
         {
             "volume_id": 9,
             "short_name": "撒上",
             "full_name": "撒母耳记上",
             "is_new": 0,
-            "chapter_number": 31
+            "chapter_number": 52
         },
         {
             "volume_id": 10,
             "short_name": "撒下",
             "full_name": "撒母耳记下",
             "is_new": 0,
-            "chapter_number": 24
+            "chapter_number": 33
         },
         {
             "volume_id": 11,
             "short_name": "王上",
             "full_name": "列王纪上",
             "is_new": 0,
-            "chapter_number": 22
+            "chapter_number": 31
         },
         {
             "volume_id": 12,
             "short_name": "王下",
             "full_name": "列王纪下",
             "is_new": 0,
-            "chapter_number": 25
+            "chapter_number": 29
         },
         {
             "volume_id": 13,
             "short_name": "代上",
             "full_name": "历代志上",
             "is_new": 0,
-            "chapter_number": 29
+            "chapter_number": 17
         },
         {
             "volume_id": 14,
             "short_name": "代下",
             "full_name": "历代志下",
             "is_new": 0,
-            "chapter_number": 36
+            "chapter_number": 33
         },
         {
             "volume_id": 15,
             "short_name": "拉",
             "full_name": "以斯拉记",
             "is_new": 0,
-            "chapter_number": 10
+            "chapter_number": 22
         },
         {
             "volume_id": 16,
             "short_name": "尼",
             "full_name": "尼希米记",
             "is_new": 0,
-            "chapter_number": 13
+            "chapter_number": 19
         },
         {
             "volume_id": 17,
             "short_name": "斯",
             "full_name": "以斯帖记",
             "is_new": 0,
-            "chapter_number": 10
+            "chapter_number": 14
         },
         {
             "volume_id": 18,
             "short_name": "伯",
             "full_name": "约伯记",
             "is_new": 0,
-            "chapter_number": 42
+            "chapter_number": 37
         },
         {
             "volume_id": 19,
             "short_name": "诗",
             "full_name": "诗篇",
             "is_new": 0,
-            "chapter_number": 150
+            "chapter_number": 8
         },
         {
             "volume_id": 20,
             "short_name": "箴",
             "full_name": "箴言",
             "is_new": 0,
-            "chapter_number": 31
+            "chapter_number": 35
         },
         {
             "volume_id": 21,
@@ -151,154 +151,154 @@ module.exports = [
             "short_name": "歌",
             "full_name": "雅歌",
             "is_new": 0,
-            "chapter_number": 8
+            "chapter_number": 13
         },
         {
             "volume_id": 23,
             "short_name": "赛",
             "full_name": "以赛亚书",
             "is_new": 0,
-            "chapter_number": 66
+            "chapter_number": 28
         },
         {
             "volume_id": 24,
             "short_name": "耶",
             "full_name": "耶利米书",
             "is_new": 0,
-            "chapter_number": 52
+            "chapter_number": 22
         },
         {
             "volume_id": 25,
             "short_name": "哀",
             "full_name": "耶利米哀歌",
             "is_new": 0,
-            "chapter_number": 5
+            "chapter_number": 22
         },
         {
             "volume_id": 26,
             "short_name": "结",
             "full_name": "以西结书",
             "is_new": 0,
-            "chapter_number": 48
+            "chapter_number": 31
         },
         {
             "volume_id": 27,
             "short_name": "但",
             "full_name": "但以理书",
             "is_new": 0,
-            "chapter_number": 12
+            "chapter_number": 28
         },
         {
             "volume_id": 28,
             "short_name": "何",
             "full_name": "何西阿书",
             "is_new": 0,
-            "chapter_number": 14
+            "chapter_number": 11
         },
         {
             "volume_id": 29,
             "short_name": "珥",
             "full_name": "约珥书",
             "is_new": 0,
-            "chapter_number": 3
+            "chapter_number": 32
         },
         {
             "volume_id": 30,
             "short_name": "摩",
             "full_name": "阿摩司书",
             "is_new": 0,
-            "chapter_number": 9
+            "chapter_number": 14
         },
         {
             "volume_id": 31,
             "short_name": "俄",
             "full_name": "俄巴底亚书",
             "is_new": 0,
-            "chapter_number": 1
+            "chapter_number": 21
         },
         {
             "volume_id": 32,
             "short_name": "拿",
             "full_name": "约拿书",
             "is_new": 0,
-            "chapter_number": 4
+            "chapter_number": 17
         },
         {
             "volume_id": 33,
             "short_name": "弥",
             "full_name": "弥迦书",
             "is_new": 0,
-            "chapter_number": 7
+            "chapter_number": 16
         },
         {
             "volume_id": 34,
             "short_name": "鸿",
             "full_name": "那鸿书",
             "is_new": 0,
-            "chapter_number": 3
+            "chapter_number": 15
         },
         {
             "volume_id": 35,
             "short_name": "哈",
             "full_name": "哈巴谷书",
             "is_new": 0,
-            "chapter_number": 3
+            "chapter_number": 17
         },
         {
             "volume_id": 36,
             "short_name": "番",
             "full_name": "西番雅书",
             "is_new": 0,
-            "chapter_number": 3
+            "chapter_number": 18
         },
         {
             "volume_id": 37,
             "short_name": "该",
             "full_name": "哈该书",
             "is_new": 0,
-            "chapter_number": 2
+            "chapter_number": 15
         },
         {
             "volume_id": 38,
             "short_name": "亚",
             "full_name": "撒迦利亚书",
             "is_new": 0,
-            "chapter_number": 8
+            "chapter_number": 15
         },
         {
             "volume_id": 39,
             "short_name": "玛",
             "full_name": "玛拉基书",
             "is_new": 0,
-            "chapter_number": 4
+            "chapter_number": 17
         },
         {
             "volume_id": 40,
             "short_name": "太",
             "full_name": "马太福音",
             "is_new": 1,
-            "chapter_number": 28
+            "chapter_number": 36
         },
         {
             "volume_id": 41,
             "short_name": "可",
             "full_name": "马可福音",
             "is_new": 1,
-            "chapter_number": 16
+            "chapter_number": 72
         },
         {
             "volume_id": 42,
             "short_name": "路",
             "full_name": "路加福音",
             "is_new": 1,
-            "chapter_number": 24
+            "chapter_number": 35
         },
         {
             "volume_id": 43,
             "short_name": "约",
             "full_name": "约翰福音",
             "is_new": 1,
-            "chapter_number": 211
+            "chapter_number": 31
         },
         {
             "volume_id": 44,
@@ -312,153 +312,153 @@ module.exports = [
             "short_name": "罗",
             "full_name": "罗马书",
             "is_new": 1,
-            "chapter_number": 16
+            "chapter_number": 23
         },
         {
             "volume_id": 46,
             "short_name": "林前",
             "full_name": "哥林多前书",
             "is_new": 1,
-            "chapter_number": 16
+            "chapter_number": 40
         },
         {
             "volume_id": 47,
             "short_name": "林后",
             "full_name": "哥林多后书",
             "is_new": 1,
-            "chapter_number": 13
+            "chapter_number": 18
         },
         {
             "volume_id": 48,
             "short_name": "加",
             "full_name": "加拉太书",
             "is_new": 1,
-            "chapter_number": 6
+            "chapter_number": 18
         },
         {
             "volume_id": 49,
             "short_name": "弗",
             "full_name": "以弗所书",
             "is_new": 1,
-            "chapter_number": 6
+            "chapter_number": 24
         },
         {
             "volume_id": 50,
             "short_name": "腓",
             "full_name": "腓立比书",
             "is_new": 1,
-            "chapter_number": 4
+            "chapter_number": 30
         },
         {
             "volume_id": 51,
             "short_name": "西",
             "full_name": "歌罗西书",
             "is_new": 1,
-            "chapter_number": 4
+            "chapter_number": 29
         },
         {
             "volume_id": 52,
             "short_name": "帖前",
             "full_name": "帖撒罗尼迦前书",
             "is_new": 1,
-            "chapter_number": 5
+            "chapter_number": 10
         },
         {
             "volume_id": 53,
             "short_name": "帖后",
             "full_name": "帖撒罗尼迦后书",
             "is_new": 1,
-            "chapter_number": 3
+            "chapter_number": 17
         },
         {
             "volume_id": 54,
             "short_name": "提前",
             "full_name": "提摩太前书",
             "is_new": 1,
-            "chapter_number": 6
+            "chapter_number": 21
         },
         {
             "volume_id": 55,
             "short_name": "提后",
             "full_name": "提摩太后书",
             "is_new": 1,
-            "chapter_number": 4
+            "chapter_number": 18
         },
         {
             "volume_id": 56,
             "short_name": "多",
             "full_name": "提多书",
             "is_new": 1,
-            "chapter_number": 3
+            "chapter_number": 16
         },
         {
             "volume_id": 57,
             "short_name": "门",
             "full_name": "腓利门书",
             "is_new": 1,
-            "chapter_number": 1
+            "chapter_number": 25
         },
         {
             "volume_id": 58,
             "short_name": "来",
             "full_name": "希伯来书",
             "is_new": 1,
-            "chapter_number": 13
+            "chapter_number": 20
         },
         {
             "volume_id": 59,
             "short_name": "雅",
             "full_name": "雅各书",
             "is_new": 1,
-            "chapter_number": 5
+            "chapter_number": 27
         },
         {
             "volume_id": 60,
             "short_name": "彼前",
             "full_name": "彼得前书",
             "is_new": 1,
-            "chapter_number": 5
+            "chapter_number": 25
         },
         {
             "volume_id": 61,
             "short_name": "彼后",
             "full_name": "彼得后书",
             "is_new": 1,
-            "chapter_number": 3
+            "chapter_number": 21
         },
         {
             "volume_id": 62,
-            "short_name": "约一",
-            "full_name": "约翰一书",
+            "short_name": "约壹",
+            "full_name": "约翰壹书",
             "is_new": 1,
-            "chapter_number": 5
+            "chapter_number": 10
         },
         {
             "volume_id": 63,
-            "short_name": "约二",
-            "full_name": "约翰二书",
+            "short_name": "约贰",
+            "full_name": "约翰贰书",
             "is_new": 1,
-            "chapter_number": 1
+            "chapter_number": 13
         },
         {
             "volume_id": 64,
-            "short_name": "约三",
-            "full_name": "约翰三书",
+            "short_name": "约叁",
+            "full_name": "约翰叁书",
             "is_new": 1,
-            "chapter_number": 1
+            "chapter_number": 15
         },
         {
             "volume_id": 65,
             "short_name": "犹",
             "full_name": "犹大书",
             "is_new": 1,
-            "chapter_number": 1
+            "chapter_number": 25
         },
         {
             "volume_id": 66,
             "short_name": "启",
             "full_name": "启示录",
             "is_new": 1,
-            "chapter_number": 22
+            "chapter_number": 20
         }
     ];
